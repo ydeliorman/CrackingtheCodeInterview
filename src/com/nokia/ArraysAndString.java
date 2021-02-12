ac845872556ea44425dc26dcebf56257175e4e5e
Yalin Eren Deliorman <yalin.deliorman@gmail.com>
2021-02-12 14:45:02 +0300

    change class name

--- src/com/nokia/ArraysAndString.java
+++ src/com/nokia/ArraysAndString.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Main {
+public class ArraysAndString {
 
     public static void main(String[] args) {
         System.out.println(findUniqueChars("asyalin"));
