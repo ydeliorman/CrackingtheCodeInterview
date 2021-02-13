3b552b7f839000c3ae8ccc64a1ec45f5394e39ff
Yalin Eren Deliorman <yalin.deliorman@gmail.com>
2021-02-13 12:18:53 +0300

    LinkedList initialcommit

---
+++ src/com/nokia/LinkedLists.java
@@ -0,0 +1,64 @@
+package com.nokia;
+/*
+13.02.2021
+created by yalind
+*/
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LinkedLists {
+
+    public static void main(String[] args) {
+
+        System.out.println("removeDups :" + removeDups());
+
+        System.out.println("returnKthToLast :" + returnKthToLast());
+
+        System.out.println("deleteMiddleNode :" + deleteMiddleNode());
+
+        System.out.println("partition :" + partition());
+
+        System.out.println("sumList :" + sumList());
+
+        System.out.println("palindrome :" + palindrome());
+
+        System.out.println("intersection :" + intersection());
+
+        System.out.println("loopDetection :" + loopDetection());
+
+    }
+
+    private static String removeDups() {
+        return null;
+    }
+
+    private static String returnKthToLast() {
+        return null;
+    }
+
+    private static String deleteMiddleNode() {
+        return null;
+    }
+
+    private static String partition() {
+        return null;
+    }
+
+    private static String sumList() {
+        return null;
+    }
+
+    private static String palindrome() {
+        return null;
+    }
+
+    private static String intersection() {
+        return null;
+    }
+
+    private static String loopDetection() {
+        return null;
+    }
+
+}
